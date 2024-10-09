630476ff9e31d36ed87bf460ce7d47da872538ee
kennethdevpc <kennethdevpc@gmail.com>
2024-10-09 14:59:41 -0600

    repiar fail

--- React-form/src/App.tsx
+++ React-form/src/App.tsx
@@ -4,11 +4,12 @@ import './App.css';
 function App() {
   const nombre = useRef<HTMLInputElement>(null);
 
-  const handleSubmit = (e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    const form = e.target.className.valueOf;
-    console.log(form);
-  };
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault(); // Previene la recarga de la página
+    const form = event.target as HTMLFormElement; // Obtenemos el formulario desde el evento
+    const input = form.elements.namedItem('lastname') as HTMLInputElement; // Obtenemos el campo de apellido
+    console.log('el input', input.value); // Mostramos el valor del input en la consola
+  }
 
   return (
     <>
