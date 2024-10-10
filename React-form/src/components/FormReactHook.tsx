165054c17ef89030fbf5f51b5cae30fdfc34fa9d
kennethdevpc <kennethdevpc@gmail.com>
2024-10-10 11:52:58 -0600

    formulario react-hook-form con ZOG aun sin resolver

--- React-form/src/components/FormReactHook.tsx
+++ React-form/src/components/FormReactHook.tsx
@@ -1,30 +1,59 @@
 import React from 'react';
 import { FieldValues, useForm } from 'react-hook-form';
+import { userSchema } from '../schemas/user';
 
 type Props = {};
-
+type Form = { name: string; lastName: string; age: number; email: string; password: string };
 function FormReactHook({}: Props) {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm<Form>();
   console.log('errores son', errors);
 
-  const onSubmit = (data: FieldValues) =>
-    console.log("informacion Obtenida desde el 'register'", data);
+  const onSubmit = (data: Form) => {
+    try {
+      //--------Uso un try-catch para manejar la validación con ZOD
+      const result = userSchema.parse(data); //----uso el método `parse` del esquema ZOD
+      console.log('Datos validados sin errores', result);
+    } catch (error) {
+      //--------Capturo el error si el esquema no es válido
+      // Para solucionar el error, se debe tipar explícitamente la variable error como 'Error' para que TypeScript lo reconozca como objeto de error.
+      console.log(`${(error as Error).message}`, 'jjjjjj');
 
+      //--------Verifico si el error contiene 'errors' para más detalles
+      if (error instanceof Error && 'errors' in error) {
+        console.log('Datos del formulario inválidos', error.message);
+      } else {
+        console.log('Ocurrió un error desconocido', error);
+      }
+    }
+  };
   return (
     <div>
-      <form action="" onSubmit={handleSubmit(onSubmit)}>
-        <input
-          type="text"
-          id="nombre"
-          {...register('nombre', {
-            required: { value: true, message: 'Este campo es obligatorio' },
-          })}
-        />
-        {errors.nombre && <span>{errors?.nombre?.message?.toString()}</span>}
+      <form
+        action=""
+        onSubmit={handleSubmit(onSubmit)}
+        style={{ display: 'flex', color: 'red', flexDirection: 'column' }}
+      >
+        <label htmlFor="">Name</label>
+        <input type="text" id="name" {...register('name')} />
+        {errors.name && <span>{errors?.name?.message?.toString()}</span>}
+        <label htmlFor="">Last name</label>
+        <input type="text" id="lastName" {...register('lastName')} />
+        {errors.lastName && <span>{errors?.lastName?.message?.toString()}</span>}
+        <label htmlFor="">Age</label>
+        <input type="text" id="age" {...register('age')} />
+        {errors.age && <span>{errors?.age?.message?.toString()}</span>}
+        <label htmlFor="">Email</label>
+        <input type="text" id="email" {...register('email')} />
+        {errors.email && <span>{errors?.email?.message?.toString()}</span>}
+        <label htmlFor="">Password</label>
+        <input type="text" id="password" {...register('password')} />
+        {errors.password && <span>{errors?.password?.message?.toString()}</span>}
+        <br />
+        <button type="submit">Save</button>
       </form>
     </div>
   );
---
+++ React-form/src/schemas/user.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod';
+
+export const userSchema = z.object({
+  name: z
+    .string()
+    .min(3, { message: 'longitud minima 3' }) //----mínimo de 3 caracteres
+    .max(20), //----máximo de 20 caracteres
+  lastName: z
+    .string()
+    .min(3, { message: 'longitud minima 3' }) //----mínimo de 3 caracteres
+    .max(20), //----máximo de 20 caracteres
+  age: z
+    .number({ coerce: true, invalid_type_error: 'el campo debe ser numerico' }) //----coerce convierte valores a número
+    .min(1, { message: 'el campo es requerido' })
+    .refine((val) => val >= 18, {
+      message: 'Debe ser mayor de edad',
+    }),
+  email: z.string().email(),
+  password: z
+    .string()
+    .refine(
+      (password) => /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{3,}$/.test(password),
+      { message: 'password invalida' }
+    ),
+});
