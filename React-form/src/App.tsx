47690754e6c11a7c09e6ce75ea4e8f23ad2c01dc
kennethdevpc <kennethdevpc@gmail.com>
2024-10-12 07:07:09 -0600

    repairone

--- React-form/src/App.tsx
+++ React-form/src/App.tsx
@@ -1,15 +1,7 @@
-import { FormEvent } from 'react';
 import './App.css';
 import FormReactHook from './components/FormReactHook';
 
 function App() {
-  function handleSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault(); // Previene la recarga de la página
-    const form = event.target as HTMLFormElement; // Obtenemos el formulario desde el evento
-    const input = form.elements.namedItem('lastname') as HTMLInputElement; // Obtenemos el campo de apellido
-    console.log('el input', input.value); // Mostramos el valor del input en la consola
-  }
-
   return (
     <>
       <FormReactHook></FormReactHook>
